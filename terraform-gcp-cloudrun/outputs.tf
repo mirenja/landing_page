6ab701cc4843a0516d25d382873b31a116b13297
mirenja <gillian.kenyani@code.berlin>
2025-07-28 10:54:45 +0200

    validated outputs.tf

--- terraform-gcp-cloudrun/outputs.tf
+++ terraform-gcp-cloudrun/outputs.tf
@@ -8,8 +8,7 @@ output "service_name" {
   value       = google_cloud_run_service.service.name
 }
 
-
 output "artifact_registry_repo_url" {
   description = "The Docker Artifact Registry repository URL"
-  value = "${google_artifact_registry_repository.docker_repo.location}-docker.pkg.dev/${google_artifact_registry_repository.docker_repo.project}/${google_artifact_registry_repository.docker_repo.repository_id}"
+  value = var.create_repo ? "${google_artifact_registry_repository.docker_repo[0].location}-docker.pkg.dev/${google_artifact_registry_repository.docker_repo[0].project}/${google_artifact_registry_repository.docker_repo[0].repository_id}" : ""
 }
