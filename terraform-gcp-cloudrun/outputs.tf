ed06ce639a59d179f582145baf264fee01e94140
mirenja <gillian.kenyani@code.berlin>
2025-08-08 10:06:35 +0200

    fix:updated the output.tf file -removed k8ns

--- terraform-gcp-cloudrun/outputs.tf
+++ terraform-gcp-cloudrun/outputs.tf
@@ -12,14 +12,3 @@ output "artifact_registry_repo_url" {
   description = "The Docker Artifact Registry repository URL"
   value = var.create_repo ? "${google_artifact_registry_repository.docker_repo[0].location}-docker.pkg.dev/${google_artifact_registry_repository.docker_repo[0].project}/${google_artifact_registry_repository.docker_repo[0].repository_id}" : ""
 }
-
-
-output "gke_cluster_name" {
-  description = "The name of the GKE cluster"
-  value       = google_container_cluster.gke_cluster.name
-}
-
-output "gke_endpoint" {
-  description = "The endpoint of the GKE cluster"
-  value       = google_container_cluster.gke_cluster.endpoint
-}
